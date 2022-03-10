d3b499c824ae6c95feb23561947abc059006f586
Anjani <pranav.ch.12@outlook.com>
2022-03-10 11:26:48 +0100

    Added Index Page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
-const defaultViewProperties = { year: new Date().getFullYear() };
+const render_index = (req, res) => res.render('index', { title: 'Home', year: new Date().getFullYear() });
+const render_about = (req, res) => res.render('about', { title: 'About', year: new Date().getFullYear() });
+const render_docs = (req, res) => res.render('docs', { title: 'Docs', year: new Date().getFullYear() });
+const render_examples = (req, res) => res.render('examples', { title: 'Examples', year: new Date().getFullYear() });
+const render_404 = (req, res) => res.status(404).render('404', { title: '404', year: new Date().getFullYear() });
 
-const render_index = (req, res) => res.render('index', { title: 'Home', ...defaultViewProperties });
-const render_404 = (req, res) => res.status(404).render('404', { title: '404', ...defaultViewProperties });
-
-module.exports = { render_index, render_404 };
+module.exports = { render_index, render_about, render_docs, render_examples, render_404 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express');
 
-const { render_index, render_404 } = require('../controllers/viewController.js');
+const { render_index, render_about, render_docs, render_examples, render_404 } = require('../controllers/viewController.js');
 
 const router = Router();
 
 router.get('/', render_index);
+router.get('/about', render_about);
+router.get('/docs', render_docs);
+router.get('/examples', render_examples);
+
+router.get('/download/pdf', (req, res) => res.download('src/docs/nodejs_brukermanual.pdf'));
 
 router.use(render_404);
 
